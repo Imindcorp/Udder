671a0b51638ed77628a9125e2cdeec39bada1a32
Imindcorp <s1lentvip@mail.ru>
2021-12-12 21:16:27 +0300

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Ilnur Mindubayev on 12.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
